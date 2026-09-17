10793e725964cfd4513105d839fdc5460cd88c0b
deepakboharachhetri <dbchhetri21113@gmail.com>
2026-09-17 15:32:19 +0545

    feat(permissions): create permission to detect staff

---
+++ accounts/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsStaff(BasePermission):
+    """
+    Allow access only to staff users.
+    """
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated and request.user.is_staff
